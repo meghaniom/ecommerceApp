218bd474939eba426194d3701e2fab2853629362
om meghani <meghanio264@gmail.com>
2025-07-14 15:25:17 +0530

    code commi

--- clients/src/routes/AppRoutes.jsx
+++ clients/src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {  BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from '../component/auth/Signup'
 import Login from '../component/auth/Login'
+import Header from '../component/comman/Header'
 const AppRoutes = () => {
   return (
    <BrowserRouter>
    <Routes>
     <Route path='/' element = {<Signup/>} />
     <Route path='/login' element ={<Login/>} />
+    <Route path='/header' element={<Header/>} />
    </Routes>
    </BrowserRouter>
   )
