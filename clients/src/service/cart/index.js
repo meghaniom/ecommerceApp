6839795137e3a0029b0da9da42abfc315c800778
om meghani <meghanio264@gmail.com>
2025-07-16 12:32:42 +0530

    the frontend side the error soul

--- clients/src/service/cart/index.js
+++ clients/src/service/cart/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../config/constans";
-import { data } from "react-router-dom";
+// import { data } from "react-router-dom";
 
  export  const addCart = async (data) => {
     try {
@@ -20,37 +20,38 @@ import { data } from "react-router-dom";
     }
  };
 
-export const removeCart = async (productId) => {
-  try {
-    const token = localStorage.getItem("token");
+  export const removeCart = async (productId) => {
+    try {
+        const token = localStorage.getItem("token");
 
-    const response = await axios.delete(`${API_BASE_URL}/cart/removeCart`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      data: { productId }, 
-    });
+        const response = await axios.delete(`${API_BASE_URL}/cart/removeCart`, {
+            headers : {
+                Authorization : `Bearer ${token}`,
+            },
+            data : {productId},
+        });
+        console.log(response.data);
+        return response.data.message|| "Product removed successfully";
+    }
+    catch(error) {
+        console.log(error);
+        return error.response.data.message || "Something went wrong";
+    }
+  };
 
-    return response.data.message;
-  } catch (error) {
-    console.log(error);
-    return error.response?.data?.message || "Something went wrong";
-  }
-};
-
-   export const getCart = async () => {
-  try {
-    const token = localStorage.getItem("token");
-    const response = await axios.get(`${API_BASE_URL}/cart/getCart`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return { cartItems: [], totalPrice: 0, error: "Something went wrong" };
-  }
-};
+   export const getCart = async()=> {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(`${API_BASE_URL}/cart/getCart`, {
+            headers : {
+                Authorization : `Bearer ${token}`,
+            }
+        });
+        console.log(response.data);
+        return response.data;
+    }
+    catch(error) {
+        console.log(error);
+       return { cartItems: [], totalPrice: 0, error: "Something went wrong" };
+    }
+   };
